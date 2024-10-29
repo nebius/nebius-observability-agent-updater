78409197a33b305479e080969e3f9b4d22f7df16
Valery Bitsoev <valerybitsoev@nebius.com>
2024-10-29 10:20:09 +0100

    fix

--- debian/usr/sbin/nebius-update-repo.sh
+++ debian/usr/sbin/nebius-update-repo.sh
@@ -3,4 +3,4 @@ set -ex
 listFIle=$(grep -rl "https://dr.nebius.cloud/" /etc/apt/sources.list.d |head | sed s'|/etc/apt/||g')
 sudo apt update -o Dir::Etc::sourcelist="$listFIle" \
                 -o Dir::Etc::sourceparts="-" \
-                -o APT::Get::List-Cleanup="0" 2>&1| tee -a "$LOG_FILE"
+                -o APT::Get::List-Cleanup="0"
